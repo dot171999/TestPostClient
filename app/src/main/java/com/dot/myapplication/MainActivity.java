239c67ad138a29e1695682a09aa1b315caf3ee14
dot171999 <agsharma98@gmail.com>
2019-08-12 20:08:59 +0530

    added post, auto dns ip

--- app/src/main/java/com/dot/myapplication/MainActivity.java
+++ app/src/main/java/com/dot/myapplication/MainActivity.java
@@ -1,7 +1,19 @@
 package com.dot.myapplication;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.DhcpInfo;
+import android.net.LinkProperties;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,31 +21,56 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    String ipAddress ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        WifiManager wifiManager= (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        if(connectivityManager.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI) {
+            Log.i("myType ", "wifi");
+            DhcpInfo d = wifiManager.getDhcpInfo();
+            int address =wifiManager.getDhcpInfo().dns1;
+
+            ipAddress = ((address & 0xFF)
+                    + "." + ((address >> 8) & 0xFF)
+                    + "." + ((address >> 16) & 0xFF)
+                    + "." + ((address >> 24) & 0xFF));
+
+            Log.e("#######DNS", ipAddress+"");
+        }
+
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendWorkPostRequest();
+                sendWorkPostRequest2();
             }
         });
     }
@@ -41,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
     private void sendWorkPostRequest() {
 
         try {
-            String URL = "http://localhost:3000";
+            String URL = "http://"+ipAddress+":3000";
             JSONObject jsonBody = new JSONObject();
 
             jsonBody.put("event", "StartBlink");
@@ -77,6 +114,72 @@ public class MainActivity extends AppCompatActivity {
             requestQueue.add(jsonObjectRequest);
 
 
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendWorkPostRequest1() {
+        //String URL = "http://localhost:3000";
+        String URL = "http://"+ipAddress+":3000";
+        StringRequest jsonArrayRequest = new StringRequest(URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("InspectionImg Response", response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(
+                10000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void sendWorkPostRequest2(){
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "http://"+ipAddress+":3000";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("event", "StartBlink");
+            jsonBody.put("value", "hello");
+            final String requestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+            };
+
+            requestQueue.add(stringRequest);
         } catch (JSONException e) {
             e.printStackTrace();
         }
