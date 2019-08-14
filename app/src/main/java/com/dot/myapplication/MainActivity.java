07526f15a11c44ef4fbe933233775c87808d52f9
dot171999 <agsharma98@gmail.com>
2019-08-15 01:17:20 +0530

    added webSocket

--- app/src/main/java/com/dot/myapplication/MainActivity.java
+++ app/src/main/java/com/dot/myapplication/MainActivity.java
@@ -1,46 +1,26 @@
 package com.dot.myapplication;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.ConnectivityManager;
-import android.net.DhcpInfo;
-import android.net.LinkProperties;
-import android.net.Network;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.Formatter;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.UnsupportedEncodingException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import static com.dot.myapplication.Client.webSocketClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,26 +31,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        WifiManager wifiManager= (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        if(connectivityManager.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI) {
-            Log.i("myType ", "wifi");
-            DhcpInfo d = wifiManager.getDhcpInfo();
-            int address =wifiManager.getDhcpInfo().dns1;
-
-            ipAddress = ((address & 0xFF)
-                    + "." + ((address >> 8) & 0xFF)
-                    + "." + ((address >> 16) & 0xFF)
-                    + "." + ((address >> 24) & 0xFF));
-
-            Log.e("#######DNS", ipAddress+"");
-        }
+        Thread thread=new Thread(new Client(MainActivity.this));
+        thread.start();
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendWorkPostRequest2();
+
+                JSONObject jsonBody = new JSONObject();
+                Log.e("event","sending event on click");
+                try {
+                    jsonBody.put("event", "BlinkDetection-Sebastian");
+                    jsonBody.put("value", "hello");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                String requestBody = jsonBody.toString();
+                webSocketClient.send(requestBody);
             }
         });
     }
